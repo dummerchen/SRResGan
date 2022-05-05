0a08581cf6d700415ac970eea34f93fa75f9a15e
dummerchen <1179502349@qq.com>
2022-05-05 10:59:10 +0800

    fix bug

--- bsd_dataset.py
+++ bsd_dataset.py
@@ -11,26 +11,13 @@ from torchvision import transforms
 import os
 from glob import glob
 import cv2
+from torchvision.transforms import functional
 class BSD_DataSets(Dataset):
     def __init__(self,path,train_or_val='train',scale_factor=4):
         self.image_paths=glob(os.path.join(path,train_or_val,'*.jpg'))
         self.train_or_val=train_or_val
         self.scale_factor=scale_factor
-        self.transforms={
-            'train':
-                transforms.Compose([
-                transforms.ToPILImage(),
-                transforms.RandomCrop(size=96),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-            ]),
-            'val':transforms.Compose([
-                transforms.ToPILImage(),
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-            ])
-        }
+
 
     def __len__(self):
         return len(self.image_paths)
@@ -39,11 +26,39 @@ class BSD_DataSets(Dataset):
         image=cv2.imread(self.image_paths[item])
         image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
         # c,h,w
-        hr=self.transforms[self.train_or_val](image)
+        if self.train_or_val=='val':
+            c, h, w = image.shape
+            self.hscale = h // self.scale_factor
+            self.wscale = w // self.scale_factor
+        else:
+            self.hscale=96//self.scale_factor
+            self.wscale=96//self.scale_factor
+        self.transforms = {
+            'train': transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.RandomCrop(size=96),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+            ]),
+            'lr': transforms.Compose([
+                transforms.Resize(size=[self.hscale, self.wscale],interpolation=functional.InterpolationMode.BICUBIC),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+            ]),
+            'hr': transforms.Compose([
+                # -1~1
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+            ]),
+            # 验证集尽可能取最大为高分辨率
+            'val': transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.CenterCrop(size=(self.hscale * self.scale_factor, self.wscale * self.scale_factor)),
+                transforms.ToTensor(),
+            ])
+        }
 
-        c,h,w=hr.shape
-        lr=transforms.Resize(size=[h//self.scale_factor,w//self.scale_factor])(hr)
-        hr=transforms.CenterCrop(size=(h//self.scale_factor*self.scale_factor,w//self.scale_factor*self.scale_factor))(hr)
+        image=self.transforms[self.train_or_val](image)
+        hr=self.transforms['hr'](image)
+        lr=self.transforms['lr'](image)
 
         return lr,hr
 
